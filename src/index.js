7b5d966ac1cdbf0413bd5f7c8fdb39641cdf639f
Danilo Maia <dcymaia@gmail.com>
2017-07-29 10:13:06 -0300

    wip - fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MeuComponente from './components/MeuComponente';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<MeuComponente />, document.getElementById('root'));
+var  MeuComponente = require('./components/MeuComponente.js');
+
+ReactDOM.render(<MeuComponente />, document.getElementById('app'));
 registerServiceWorker();
